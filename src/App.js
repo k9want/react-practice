c1b69f52511fe0976c6ce986d25f8667e2517195
qma <zndwlrnjr26@daum.net>
2022-09-03 23:35:46 +0900

    ✨ Implement Login Component using useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
-import Clock from './clock/Clock';
-import CommentList from './comment/CommentList';
+import './App.css'
+import Clock from './clock/Clock'
+import CommentList from './comment/CommentList'
+import Login from './login/Login'
 
 function App() {
   return (
     <div className="App">
       <Clock />
+      <Login />
       <CommentList />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
